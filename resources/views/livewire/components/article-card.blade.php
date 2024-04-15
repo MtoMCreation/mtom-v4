750baa21033d32237794ae0c376eabff1b7f282a
julien <julien.boullet13@gmail.com>
2024-04-14 22:57:07 -0400

    modifs layout

--- resources/views/livewire/components/article-card.blade.php
+++ resources/views/livewire/components/article-card.blade.php
@@ -1,9 +1,14 @@
 <div class="card rounded overflow-hidden bg-gray-100">
     <div
-        style="background-image: url({{ Storage::url($article->image) }}); background-size: cover; background-position: center; height: 200px;"
-        class="">
+        style="
+            background-image: url({{ Storage::url($article->image) }});
+            background-size: cover;
+            background-position: center;
+            height: 320px;">
     </div>
 
-    <h2>{{ $article->title }}</h2>
+    <div class="p-10">
+        <h2>{{ $article->title }}</h2>
+    </div>
 
 </div>
