57861312cabf40bb7ed249850d88ac4fb1f18a68
julien <julien.boullet13@gmail.com>
2024-04-14 22:42:49 -0400

    modifs layout

--- resources/views/livewire/pages/articles.blade.php
+++ resources/views/livewire/pages/articles.blade.php
@@ -7,7 +7,7 @@
     <h1>Page des articles</h1>
 
     <div class="mt-10">
-        <div class="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             @foreach($articles as $article)
                 <a href="{{ route('article-show', $article) }}" wire:navigate>
                     <livewire:components.article-card :article="$article" :key="$article->id" lazy/>
