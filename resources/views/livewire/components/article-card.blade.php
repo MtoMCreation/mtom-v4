209b98421ed11713657760d9a6dc4a02b2830b09
julien <julien.boullet13@gmail.com>
2024-04-14 22:11:37 -0400

    debug affichage des images

--- resources/views/livewire/components/article-card.blade.php
+++ resources/views/livewire/components/article-card.blade.php
@@ -1,8 +1,7 @@
 <div class="card">
     <div
         style="
-        height: 200px;
-        background-image: url("{{ asset('storage/' . $article->image) }}")">
+        height: 200px; background-image: url({{ $article->image }})">
     </div>
 
 {{--    <img src="{{ asset('storage/' . $article->image) }}" style="" alt="">--}}
