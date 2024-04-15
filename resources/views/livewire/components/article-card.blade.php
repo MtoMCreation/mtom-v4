4affc715dd82454db9c3d3795d7f12ee7890a909
julien <julien.boullet13@gmail.com>
2024-04-14 22:27:00 -0400

    debug affichage des images

--- resources/views/livewire/components/article-card.blade.php
+++ resources/views/livewire/components/article-card.blade.php
@@ -4,7 +4,8 @@
             height: 330px;
             background-image: url({{ Storage::url($article->image) }});
             background-repeat: no-repeat;
-            background-position: center;">
+            background-position: center;
+            background-size: cover;">
     </div>
 
     <h2>{{ $article->title }}</h2>
